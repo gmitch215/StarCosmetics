f4fead7eb63aeaab573414843c765c0654e82f09
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-09-04 21:45:21 -0500

    Create Generator.java

---
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/api/Generator.java
@@ -0,0 +1,43 @@
+package me.gamercoder215.starcosmetics.api;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public final class Generator {
+
+    public static ItemStack getGUIBackground() {
+        ItemStack item = StarConfig.isLegacy() ? new ItemStack(Material.matchMaterial("STAINED_GLASS_PANE"), 1, (short) 7) : new ItemStack(Material.matchMaterial("GRAY_STAINED_GLASS_PANE"));
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(" ");
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    public static Inventory genGUI(int size, String name) {
+        return genGUI(size, name, null);
+    }
+
+    public static Inventory genGUI(int size, String name, InventoryHolder holder) {
+        if (size < 9 || size > 54) return null;
+        if (size % 9 > 0) return null;
+
+        Inventory inv = Bukkit.createInventory(holder, size, name);
+        ItemStack bg = getGUIBackground();
+
+        if (size < 27) return inv;
+
+        for (int i = 0; i < 9; i++) inv.setItem(i, bg);
+        for (int i = size - 9; i < size; i++) inv.setItem(i, bg);
+        for (int i = 1; i < Math.floor((double) size / 9D) - 1; i++) {
+            inv.setItem(i * 9, bg);
+            inv.setItem(((i + 1) * 9) - 1, bg);
+        }
+
+        return inv;
+    }
+
+}
