93dd0df06153b27afb65389269069636eae6306c
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-09-04 21:45:30 -0500

    Create Config for interaction in Wrappers

---
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/api/StarConfig.java
@@ -0,0 +1,145 @@
+package me.gamercoder215.starcosmetics.api;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public interface StarConfig {
+
+    static Plugin getPlugin() {
+        return Bukkit.getPluginManager().getPlugin("StarCosmetics");
+    }
+
+    static StarConfig getConfig() {
+        return (StarConfig) getPlugin();
+    }
+
+    static void print(Throwable t) {
+        getLogger().severe(t.getClass().getSimpleName());
+        getLogger().severe("--------------------------");
+        getLogger().severe(t.getMessage());
+        for (StackTraceElement s : t.getStackTrace()) getLogger().severe(s.toString());
+    }
+
+    static FileConfiguration getConfiguration() {
+        if (!getConfigurationFile().exists()) {
+            try {
+                getConfigurationFile().createNewFile();
+            } catch (IOException e) {
+                print(e);
+            }
+        }
+
+        return getPlugin().getConfig();
+    }
+
+    static File getDataFolder() {
+        return getPlugin().getDataFolder();
+    }
+
+    static Logger getLogger() {
+        return getPlugin().getLogger();
+    }
+
+    static File getConfigurationFile() {
+        return new File(getDataFolder(), "config.yml");
+    }
+
+    static FileConfiguration loadConfig() {
+        FileConfiguration config = getConfiguration();
+
+        if (!config.isString("language")) config.set("language", "en");
+
+        try {
+            config.save(getConfigurationFile());
+        } catch (IOException e) {
+            print(e);
+        }
+
+        return config;
+    }
+
+    String getLanguage();
+
+    String get(String key);
+
+    String getMessage(String key);
+
+    static boolean isLegacy() {
+        return ServerVersion.getCurrent() == ServerVersion.LEGACY;
+    }
+
+    static String getServerVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
+    }
+
+    enum ServerVersion {
+        LEGACY(1,
+                "1_9_R1",
+                "1_9_R2",
+                "1_10_R1",
+                "1_11_R1",
+                "1_12_R1"),
+
+        CONTEMPORARY(2,
+                "1_13_R1",
+                "1_13_R2",
+                "1_14_R1",
+                "1_15_R1",
+                "1_16_R1",
+                "1_16_R2",
+                "1_16_R3",
+                "1_17_R1",
+                "1_18_R1",
+                "1_18_R2",
+                "1_19_R1"),
+
+        UNKNOWN(1)
+        ;
+
+        private final String[] compatibleVersions;
+
+        private final int cmdV;
+
+        ServerVersion(int cmdV, String... compatibleVersions) {
+            this.compatibleVersions = compatibleVersions;
+            this.cmdV = cmdV;
+        }
+
+        public int getCommandVersion() {
+            return this.cmdV;
+        }
+
+        public boolean isCompatible() {
+            for (String version : compatibleVersions) {
+                if (version.equals(getServerVersion())) return true;
+            }
+            return false;
+        }
+
+        public static boolean isAllCompatible() {
+            for (ServerVersion version : values()) {
+                if (version.isCompatible()) return true;
+            }
+            return false;
+        }
+
+        public static ServerVersion getByVersion(String version) {
+            for (ServerVersion srvV : values()) {
+                for (String v : srvV.compatibleVersions) {
+                    if (v.equals(version)) return srvV;
+                }
+            }
+            return UNKNOWN;
+        }
+
+        public static ServerVersion getCurrent() {
+             return getByVersion(getServerVersion());
+        }
+
+    }
+}
--- plugin/src/main/java/me/gamercoder215/starcosmetics/StarCosmetics.java
+++ plugin/src/main/java/me/gamercoder215/starcosmetics/StarCosmetics.java
@@ -1,12 +1,89 @@
 package me.gamercoder215.starcosmetics;
 
+import me.gamercoder215.starcosmetics.api.StarConfig;
+import me.gamercoder215.starcosmetics.wrapper.CommandWrapper;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class StarCosmetics extends JavaPlugin {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+public final class StarCosmetics extends JavaPlugin implements StarConfig {
+
+    private boolean checkCompatible() {
+        if (!ServerVersion.isAllCompatible()) {
+            getLogger().severe("StarCosmetics is not compatible with: " + Bukkit.getBukkitVersion());
+            Bukkit.getPluginManager().disablePlugin(this);
+            return false;
+        }
+
+        return true;
+    }
+
+    private static FileConfiguration config;
+
+    @Override
     public void onEnable() {
+        if (!checkCompatible()) return;
+        saveDefaultConfig();
+
+        config = StarConfig.loadConfig();
+        getLogger().info("Loaded Files...");
+
+        getCommandWrapper();
 
         getLogger().info("Done!");
     }
+
+    // Config Implementation
+
+    @Override
+    public String getLanguage() {
+        return config.getString("language", "en");
+    }
+
+    @Override
+    public String get(String key) {
+        Properties p = new Properties();
+        String lang = getLanguage().equalsIgnoreCase("en") ? "" : getLanguage();
+
+        try (InputStream str = getClass().getResourceAsStream("/starcosmetics" + lang + ".properties")) {
+            p.load(str);
+            str.close();
+            return ChatColor.translateAlternateColorCodes('&', p.getProperty(key, "Unknown Value"));
+        } catch (IOException e) {
+            StarConfig.print(e);
+            return "Unknown Value";
+        }
+    }
+
+    @Override
+    public String getMessage(String key) {
+        return get("plugin.prefix") + get(key);
+    }
+
+    // Other Utilities
+
+    public static void print(Throwable t) {
+        StarConfig.print(t);
+    }
+
+    public static CommandWrapper getCommandWrapper() {
+        StarCosmetics plugin = StarCosmetics.getPlugin(StarCosmetics.class);
+        int cmdV = ServerVersion.getCurrent().getCommandVersion();
+
+        try {
+            return (CommandWrapper) Class.forName("me.gamercoder215.starcosmetics.wrapper.CommandWrapperV" + cmdV).getConstructor(Plugin.class).newInstance(plugin);
+        } catch (ReflectiveOperationException e) {
+            print(e);
+            return null;
+        }
+    }
+
+
 
 }
