126b9c4ead5381ad45246715ab769a672591bd75
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-09-18 11:39:48 -0500

    Update BaseTrail.java
    - Create ArrowTrail Draft

--- abstraction/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/BaseTrail.java
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/BaseTrail.java
@@ -1,17 +1,54 @@
 package me.gamercoder215.starcosmetics.api.cosmetics;
 
-import java.util.function.BiConsumer;
-
+import me.gamercoder215.starcosmetics.api.StarConfig;
+import me.gamercoder215.starcosmetics.wrapper.Wrapper;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.function.BiConsumer;
 
 public enum BaseTrail implements BiConsumer<Projectile, Object> {
     
     ARROW_TRAIL((p, o) -> {
-        // TODO
-    });
-    ;
+        Location loc = p.getLocation();
 
-    private BiConsumer<Projectile, Object> trail;
+        if (o instanceof Particle) {
+            Particle part = (Particle) o;
+            new BukkitRunnable() {
+                public void run() {
+                    if (p.isDead() || !p.isValid()) {
+                        cancel();
+                        return;
+                    }
+
+                    p.getWorld().spawnParticle(part, loc, 1, 0, 0, 0, 0);
+                }
+            }.runTaskTimer(StarConfig.getPlugin(), 0, 2);
+        }
+
+        if (o instanceof Material || o instanceof ItemStack) {
+            ItemStack item = o instanceof Material ? new ItemStack((Material) o) : (ItemStack) o;
+            new BukkitRunnable() {
+                public void run() {
+                    if (p.isDead() || !p.isValid()) {
+                        cancel();
+                        return;
+                    }
+                    
+                    for (Player pl : p.getWorld().getPlayers()) {
+                        Wrapper.getWrapper().spawnFakeItem(pl, item, p.getLocation(), 10);
+                    }
+                }
+            }.runTaskTimer(StarConfig.getPlugin(), 0, 2);
+        }
+    });
+
+    private final BiConsumer<Projectile, Object> trail;
 
     BaseTrail(BiConsumer<Projectile, Object> runnable) {
         this.trail = runnable;
