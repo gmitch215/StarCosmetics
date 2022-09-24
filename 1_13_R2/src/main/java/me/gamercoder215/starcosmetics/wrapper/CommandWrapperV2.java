091aaca4480dbfc25396e929237bd3febf0b1e18
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-09-23 21:39:40 -0500

    Update CommandWrapperV2.java
    - Base Lamp Wrapper Draft

--- 1_13_R2/src/main/java/me/gamercoder215/starcosmetics/wrapper/CommandWrapperV2.java
+++ 1_13_R2/src/main/java/me/gamercoder215/starcosmetics/wrapper/CommandWrapperV2.java
@@ -1,8 +1,13 @@
 package me.gamercoder215.starcosmetics.wrapper;
 
 import me.gamercoder215.starcosmetics.api.StarConfig;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Description;
+import revxrsal.commands.annotation.Usage;
 import revxrsal.commands.bukkit.BukkitCommandHandler;
+import revxrsal.commands.bukkit.annotation.CommandPermission;
 
 import java.util.Locale;
 
@@ -12,8 +17,7 @@ public class CommandWrapperV2 implements CommandWrapper {
 
     public CommandWrapperV2(Plugin plugin) {
         if (handler != null) return;
-
-
+        handler = BukkitCommandHandler.create(plugin);
 
         handler.register(this);
         handler.registerBrigadier();
@@ -22,4 +26,21 @@ public class CommandWrapperV2 implements CommandWrapper {
         plugin.getLogger().info("Loaded Command Wrapper V2 (1.13+)");
     }
 
+    @Override
+    @Command({"starsettings", "ssettings", "settings", "ss"})
+    @Description("Opens the StarCosmetics settings menu.")
+    @Usage("/starsettings")
+    @CommandPermission("starcosmetics.user.settings")
+    public void settings(Player p) {
+        CommandWrapper.super.settings(p);
+    }
+
+    @Override
+    @Command({"starreload", "sreload", "sr"})
+    @Description("Reloads the StarCosmetics configuration.")
+    @Usage("/starreload")
+    @CommandPermission("starcosmetics.admin.reloadconfig")
+    public void reloadConfig(Player p) {
+        CommandWrapper.super.reloadConfig(p);
+    }
 }
