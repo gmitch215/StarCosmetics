0b0db2eaace7e1aaf988fd77ce339682ba65e9d4
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-09-23 21:39:30 -0500

    Update BaseParticleCosmetic.java
    - Create Circle Cosmetic Implementation

--- abstraction/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/BaseParticleCosmetic.java
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/BaseParticleCosmetic.java
@@ -1,25 +1,46 @@
 package me.gamercoder215.starcosmetics.api.cosmetics;
 
-import java.util.function.Consumer;
-
+import org.bukkit.Location;
 import org.bukkit.Particle;
 
-public enum BaseParticleCosmetic implements Consumer<Particle> {
+import java.util.function.BiConsumer;
+
+public enum BaseParticleCosmetic implements BiConsumer<Location, Particle> {
     
-    SMALL_RING((p -> {
-        // TODO
-    }));
+    SMALL_RING((l, p) -> circle(l, p, 10, 4)),
+
+    SMALL_DETAILED_RING((l, p) -> {
+        circle(l, p, 20, 5);
+        circle(l, p, 10, 3);
+    }),
+
+    LARGE_RING((l, p) -> circle(l, p, 20, 7)),
+
+    LARGE_DETAILED_RING((l, p) -> {
+        circle(l, p, 40, 10);
+        circle(l, p, 20, 7);
+    }),
     ;
 
-    private Consumer<Particle> particle;
+    private static void circle(Location l, Particle p, int points, double radius) {
+        for (int i = 0; i < points; i++) {
+            double angle = 2 * Math.PI * i / points;
+            l.add(radius * Math.sin(angle), 0, radius * Math.cos(angle));
+            l.getWorld().spawnParticle(p, l, 1, 0, 0, 0, 0);
+            l.subtract(radius * Math.sin(angle), 0, radius * Math.cos(angle));
+        }
+    }
 
-    BaseParticleCosmetic(Consumer<Particle> runnable) {
+
+    private final BiConsumer<Location, Particle> particle;
+
+    BaseParticleCosmetic(BiConsumer<Location, Particle> runnable) {
         this.particle = runnable;
     }
 
     @Override
-    public void accept(Particle t) {
-        particle.accept(t);
+    public void accept(Location l, Particle t) {
+        particle.accept(l, t);
     }
 
 }
