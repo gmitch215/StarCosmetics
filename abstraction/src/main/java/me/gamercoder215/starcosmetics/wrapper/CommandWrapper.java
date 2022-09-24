12089ea398f657cfc8f803a9a9178a0ef4a6b360
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-09-23 21:39:11 -0500

    Update CommandWrapper.java
    - Add Base Settings and Reload Command

--- abstraction/src/main/java/me/gamercoder215/starcosmetics/wrapper/CommandWrapper.java
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/wrapper/CommandWrapper.java
@@ -1,28 +1,42 @@
 package me.gamercoder215.starcosmetics.wrapper;
 
 import com.google.common.collect.ImmutableMap;
+import org.bukkit.entity.Player;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public interface CommandWrapper {
 
     Map<String, List<String>> COMMANDS = ImmutableMap.<String, List<String>>builder()
-
+            .put("starsettings", Arrays.asList("ssettings", "settings", "ss"))
+            .put("starreload", Arrays.asList("sreload", "sr"))
             .build();
 
     Map<String, String> COMMAND_PERMISSION = ImmutableMap.<String, String>builder()
-
+            .put("starsettings", "starcosmetics.user.settings")
+            .put("starreload", "starcosmetics.admin.reloadconfig")
             .build();
 
     Map<String, String> COMMAND_DESCRIPTION = ImmutableMap.<String, String>builder()
-
+            .put("starsettings", "Opens the StarCosmetics settings menu.")
+            .put("starreload", "Reloads the StarCosmetics configuration.")
             .build();
 
     Map<String, String> COMMAND_USAGE = ImmutableMap.<String, String>builder()
-
+            .put("starsettings", "/starsettings")
+            .put("starreload", "/starreload")
             .build();
 
     // Command Methods
+
+    default void settings(Player p) {
+
+    }
+
+    default void reloadConfig(Player p) {
+
+    }
 
 }
