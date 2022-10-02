5bd3f83eb2e3114e7b1ed50b332e997c7490244b
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-01 19:24:40 -0500

    Update Wrapper.java

--- abstraction/src/main/java/me/gamercoder215/starcosmetics/wrapper/Wrapper.java
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/wrapper/Wrapper.java
@@ -1,6 +1,8 @@
 package me.gamercoder215.starcosmetics.wrapper;
 
+import me.gamercoder215.starcosmetics.api.StarConfig;
 import me.gamercoder215.starcosmetics.wrapper.nbt.NBTWrapper;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
@@ -9,6 +11,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public interface Wrapper {
+
+    static boolean isCompatible() {
+        return getWrapper() == null;
+    }
+
+    static String getServerVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
+    }
+
+    static Wrapper getWrapper() {
+        try {
+            return (Wrapper) Class.forName("me.gamercoder215.starcosmetics.wrapper.Wrapper" + getServerVersion())
+                    .getConstructor()
+                    .newInstance();
+        } catch (ClassNotFoundException e) { // Using unsupported version
+            return null;
+        } catch (ReflectiveOperationException e) {
+            StarConfig.print(e);
+        } catch (ArrayIndexOutOfBoundsException ignored) {} // Using test server
+
+        return null;
+    }
 
     int getCommandVersion();
 
