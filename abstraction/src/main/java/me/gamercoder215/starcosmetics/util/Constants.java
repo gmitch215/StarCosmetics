9697bad55affb2a19e9340cd0289249fef01c03f
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-01 19:24:35 -0500

    Update API Classes

---
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/util/Constants.java
@@ -0,0 +1,13 @@
+package me.gamercoder215.starcosmetics.util;
+
+import me.gamercoder215.starcosmetics.wrapper.Wrapper;
+
+import java.security.SecureRandom;
+
+public interface Constants {
+
+    SecureRandom r = new SecureRandom();
+
+    Wrapper w = Wrapper.getWrapper();
+
+}
---
+++ api/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/ParticleShape.java
@@ -0,0 +1,9 @@
+package me.gamercoder215.starcosmetics.api.cosmetics;
+
+/**
+ * Represents a StarCosmetics Particle Shape.
+ * @since 1.0.0
+ */
+public interface ParticleShape extends CosmeticKey {
+
+}
