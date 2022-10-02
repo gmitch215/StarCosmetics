5dce7a1fea5e5c685b915adda4269d4d29fae4bb
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-01 19:24:12 -0500

    Update StarCosmetics.java; Create CompletionEvents.java

--- plugin/src/main/java/me/gamercoder215/starcosmetics/StarCosmetics.java
+++ plugin/src/main/java/me/gamercoder215/starcosmetics/StarCosmetics.java
@@ -1,11 +1,16 @@
 package me.gamercoder215.starcosmetics;
 
 import me.gamercoder215.starcosmetics.api.StarConfig;
+import me.gamercoder215.starcosmetics.events.ClickEvents;
+import me.gamercoder215.starcosmetics.wrapper.Wrapper;
+import me.gamercoder215.starcosmetics.wrapper.commands.CommandWrapper;
+import me.gamercoder215.starcosmetics.wrapper.cosmetics.CosmeticSelections;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Firework;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -14,8 +19,10 @@ import java.util.Properties;
 
 public final class StarCosmetics extends JavaPlugin implements StarConfig {
 
+    private static final Wrapper w = Wrapper.getWrapper();
+
     private boolean checkCompatible() {
-        if (!ServerVersion.isAllCompatible()) {
+        if (!Wrapper.isCompatible()) {
             getLogger().severe("StarCosmetics is not compatible with: " + Bukkit.getBukkitVersion());
             Bukkit.getPluginManager().disablePlugin(this);
             return false;
@@ -39,7 +46,7 @@ public final class StarCosmetics extends JavaPlugin implements StarConfig {
         getLogger().info("Loaded Files...");
 
         registerEvents();
-        StarConfig.getCommandWrapper();
+        getCommandWrapper();
         getLogger().info("Loaded Classes...");
 
         getLogger().info("Done!");
@@ -81,6 +88,40 @@ public final class StarCosmetics extends JavaPlugin implements StarConfig {
     }
 
     // Other Utilities
+
+    public static String getServerVersion() {
+        return Wrapper.getServerVersion();
+    }
+
+    public static boolean isCompatible() {
+        return Wrapper.isCompatible();
+    }
+
+    public static CommandWrapper getCommandWrapper() {
+        if (!isCompatible()) return null;
+        int cmdV = w.getCommandVersion();
+
+        try {
+            return (CommandWrapper) Class.forName("me.gamercoder215.starcosmetics.wrapper.CommandWrapperV" + cmdV)
+                    .getConstructor(Plugin.class)
+                    .newInstance(StarConfig.getPlugin());
+        } catch (ReflectiveOperationException e) {
+            StarConfig.print(e);
+            return null;
+        }
+    }
+
+    public static CosmeticSelections getCosmeticSelections() {
+        String cosmeticV = getServerVersion().split("_")[0] + "_" + getServerVersion().split("_")[1];
+        try {
+            return (CosmeticSelections) Class.forName("me.gamercoder215.starcosmetics.wrapper.cosmetics.CosmeticSelections" + cosmeticV)
+                    .getConstructor()
+                    .newInstance();
+        } catch (ReflectiveOperationException e) {
+            StarConfig.print(e);
+            return null;
+        }
+    }
 
     public static void print(Throwable t) {
         StarConfig.print(t);
---
+++ plugin/src/main/java/me/gamercoder215/starcosmetics/events/CompletionEvents.java
@@ -0,0 +1,49 @@
+package me.gamercoder215.starcosmetics.events;
+
+import me.gamercoder215.starcosmetics.StarCosmetics;
+import me.gamercoder215.starcosmetics.api.player.StarPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import static me.gamercoder215.starcosmetics.api.player.StarPlayer.COMPLETION_NETHER_ROOF;
+
+public class CompletionEvents implements Listener {
+
+    private final StarCosmetics plugin;
+
+    public CompletionEvents(StarCosmetics plugin) {
+        this.plugin = plugin;
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onMoveAsync(PlayerMoveEvent e) {
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                Player p = e.getPlayer();
+                StarPlayer sp = new StarPlayer(p);
+                Location to = e.getTo();
+                switch (to.getWorld().getEnvironment()) {
+                    case NORMAL: {
+
+                        break;
+                    }
+                    case NETHER: {
+                        if (sp.hasCompleted(COMPLETION_NETHER_ROOF)) return;
+                        if (to.getBlockY() >= 128) sp.setCompleted(COMPLETION_NETHER_ROOF, true);
+
+                        break;
+                    }
+                }
+            }
+        }.runTaskAsynchronously(plugin);
+    }
+
+}
