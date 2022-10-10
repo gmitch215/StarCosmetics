237c9e3d5d6fbed7f105f22523e35d22ec6d932e
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-10 11:18:24 -0500

    Update StructurePoint.java

--- api/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/structure/StructurePoint.java
+++ api/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/structure/StructurePoint.java
@@ -1,15 +1,15 @@
 package me.gamercoder215.starcosmetics.api.cosmetics.structure;
 
 import org.bukkit.Location;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
 /**
  * Represents a point on a Structure.
- * @since 1.0.0
- */
-public final class StructurePoint {
+*/
+public final class StructurePoint implements Comparable<StructurePoint> {
     
     private final int x;
     private final int y;
@@ -20,7 +20,6 @@ public final class StructurePoint {
      * @param x Relative X to the Center
      * @param y Relative Y to the Center
      * @param z Relative Z to the Center
-     * @since 1.0.0
      */
     public StructurePoint(int x, int y, int z) {
         this.x = x;
@@ -31,7 +30,6 @@ public final class StructurePoint {
     /**
      * Fetches the relative X distance to the Center.
      * @return Relative X Distance
-     * @since 1.0.0
      */
     public int getX() {
         return x;
@@ -40,7 +38,6 @@ public final class StructurePoint {
     /**
      * Fetches the relative Y distance to the Center.
      * @return Relative Y Distance
-     * @since 1.0.0
      */
     public int getY() {
         return y;
@@ -49,7 +46,6 @@ public final class StructurePoint {
     /**
      * Fetches the relative Z distance to the Center.
      * @return Relative Z Distance
-     * @since 1.0.0
      */
     public int getZ() {
         return z;
@@ -59,12 +55,20 @@ public final class StructurePoint {
      * Converts this StructurePoint to a Location.
      * @param relative Center Location
      * @return Location of this StructurePoint according to the relative Location
-     * @since 1.0.0
      */
     @NotNull
     public Location toLocation(@NotNull Location relative) {
         return new Location(relative.getWorld(),
          relative.getX() + x, relative.getY() + y, relative.getZ() + z);
+    }
+
+    /**
+     * Converts this StructurePoint to a Vector, inputting its actual X, Y, and Z.
+     * @return Vector of this StructurePoint
+     */
+    @NotNull
+    public Vector toVector() {
+        return new Vector(x, y, z);
     }
 
     @Override
@@ -87,5 +91,10 @@ public final class StructurePoint {
                 ", y=" + y +
                 ", z=" + z +
                 '}';
+    }
+
+    @Override
+    public int compareTo(@NotNull StructurePoint o) {
+        return Integer.compare(this.hashCode(), o.hashCode());
     }
 }
