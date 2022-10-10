4eadbde4747f9277b235779e64a8bd5c6ec111bb
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-10 11:32:53 -0500

    Update Cosmetic.java

--- api/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/Cosmetic.java
+++ api/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/Cosmetic.java
@@ -1,10 +1,11 @@
 package me.gamercoder215.starcosmetics.api.cosmetics;
 
 import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Parent Interface for Base Cosmetics, ready for material, particle, or entity implementations.
- * @since 1.0.0m
+
  */
 public interface Cosmetic {
 
@@ -12,18 +13,21 @@ public interface Cosmetic {
      * Fetches the namespace of this Comsmetic.
      * @return Cosmetic Namespace
      */
+    @NotNull
     String getNamespace();
 
     /**
      * Fetches the display name of this cosmetic.
      * @return Display name of Cosmetic
      */
+    @NotNull
     String getDisplayName();
 
     /**
      * Fetches the icon of this cosmetic.
      * @return The icon of this cosmetic.
      */
+    @NotNull
     Material getIcon();
 
 }
