0183286bf18262374b23b3078a54db18657b3624
GamerCoder215 <gamercoder215@teaminceptus.us>
2022-10-20 14:22:17 +0000

    Rename SoundSelector.java

--- abstraction/src/main/java/me/gamercoder215/starcosmetics/util/inventory/MaterialSelector.java
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/util/inventory/MaterialSelector.java
@@ -1,20 +1,46 @@
 package me.gamercoder215.starcosmetics.util.inventory;
 
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 
 import me.gamercoder215.starcosmetics.util.StarMaterial;
 import me.gamercoder215.starcosmetics.wrapper.Wrapper;
 
-public final class SoundSelector {
+public final class MaterialSelector {
 
     private static final Wrapper w = Wrapper.getWrapper();
     
-    private SoundSelector() { throw new UnsupportedOperationException(); }
+    private MaterialSelector() { throw new UnsupportedOperationException(); }
+
+    @NotNull
+    public static Material toMaterial(Class<? extends Event> eventClass) {
+        if (Modifier.isAbstract(eventClass.getModifiers())) throw new IllegalArgumentException("Using Abstract Class");
+
+        String n = eventClass.getSimpleName().toLowerCase();
+        Material chosen = null;
+
+        if (n.contains("player")) chosen = Material.IRON_SWORD;
+        if (chosen == null && n.contains("block")) chosen = Material.DIRT;
+        if (chosen == null && n.contains("server")) chosen = StarMaterial.OBSERVER.find();
+        if (chosen == null && n.contains("inventory")) chosen = StarMaterial.CRAFTING_TABLE.find();
+        if (chosen == null && n.contains("weather")) chosen = Material.BUCKET;
+        if (chosen == null && n.contains("vehicle")) chosen = Material.MINECART;
+
+        switch (n) {
+            case "asyncplayerchatevent": return StarMaterial.COMMAND_BLOCK.find();
+            case "playerjoinevent": return StarMaterial.GRASS_BLOCK.find();
+            case "playerrespawnevent": return Material.BEACON;
+        }
+
+        if (chosen == null) chosen = Material.REDSTONE;
+        return chosen;
+    }
 
     @NotNull
     public static Material toMaterial(Sound s) {
