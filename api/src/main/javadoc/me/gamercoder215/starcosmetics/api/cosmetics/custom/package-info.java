eb1d7c78a4e263a6c10dca7db1809f8a97b9f563
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-10-21 17:53:22 -0500

    Create Sound and Custom package-infos

---
+++ api/src/main/javadoc/me/gamercoder215/starcosmetics/api/cosmetics/custom/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package related to Custom Cosmetics.
+ */
+package me.gamercoder215.starcosmetics.api.cosmetics.custom;
---
+++ api/src/main/javadoc/me/gamercoder215/starcosmetics/api/cosmetics/sound/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package related to Cosmetics that play sounds or sequences of sounds.
+ */
+package me.gamercoder215.starcosmetics.api.cosmetics.sound;
