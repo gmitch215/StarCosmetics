cec3ec8e9445e79f16753e765ddd328153579b22
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-11-05 02:28:32 -0500

    Update CosmeticEvents.java

--- plugin/src/main/java/me/gamercoder215/starcosmetics/events/CosmeticEvents.java
+++ plugin/src/main/java/me/gamercoder215/starcosmetics/events/CosmeticEvents.java
@@ -1,14 +1,16 @@
 package me.gamercoder215.starcosmetics.events;
 
+import me.gamercoder215.starcosmetics.StarCosmetics;
+import me.gamercoder215.starcosmetics.api.cosmetics.registry.CosmeticLocation;
+import me.gamercoder215.starcosmetics.api.cosmetics.trail.Trail;
+import me.gamercoder215.starcosmetics.api.cosmetics.trail.TrailType;
+import me.gamercoder215.starcosmetics.api.player.StarPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
-
-import me.gamercoder215.starcosmetics.StarCosmetics;
-import me.gamercoder215.starcosmetics.api.player.StarPlayer;
 
 public final class CosmeticEvents implements Listener {
     
@@ -25,6 +27,12 @@ public final class CosmeticEvents implements Listener {
         if (!(en.getShooter() instanceof Player)) return;
         Player p = (Player) en.getShooter();
         StarPlayer sp = new StarPlayer(p);
+
+        CosmeticLocation<?> proj = sp.getSelectedTrail(TrailType.PROJECTILE);
+        if (proj != null) ((Trail<?>) proj.getParent()).run(en, proj);
+
+        CosmeticLocation<?> sProj = sp.getSelectedTrail(TrailType.PROJECTILE_SOUND);
+        if (sProj != null) ((Trail<?>) sProj.getParent()).run(en, sProj);
     }
 
 }
