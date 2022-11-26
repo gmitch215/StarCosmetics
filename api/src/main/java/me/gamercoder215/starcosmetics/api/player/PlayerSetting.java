a783603777ae4668ab3ab8978d473094da2b1cc2
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-11-26 00:46:24 -0600

    Update PlayerSetting
    - Create Possible Values
    - Create PlayerSetting#STRUCTURE_VELOCITY

--- api/src/main/java/me/gamercoder215/starcosmetics/api/player/PlayerSetting.java
+++ api/src/main/java/me/gamercoder215/starcosmetics/api/player/PlayerSetting.java
@@ -1,10 +1,17 @@
 package me.gamercoder215.starcosmetics.api.player;
 
+import com.google.common.collect.ImmutableList;
 import me.gamercoder215.starcosmetics.api.StarConfig;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Represents a setting available to players.
@@ -17,26 +24,49 @@ public final class PlayerSetting<T> {
      */
     @SettingDescription("settings.notifications.desc")
     public static final PlayerSetting<Boolean> NOTIFICATIONS =
-            new PlayerSetting<>("notifications", "settings.notifications", boolean.class, true);
+            ofBoolean("notifications", "settings.notifications", true);
 
     /**
      * Whether the player should receive sound notifications.
      */
     @SettingDescription("settings.notifications.sound.desc")
     public static final PlayerSetting<Boolean> SOUND_NOTIFICATIONS =
-            new PlayerSetting<>("sound_notifications", "settings.notifications.sound", boolean.class, true);
+            ofBoolean("sound_notifications", "settings.notifications.sound", true);
+
+    /**
+     * Whether the player should be launched when spawning a structure, to "avoid" suffocation.
+     */
+    @SettingDescription("settings.structure_velocity.desc")
+    public static final PlayerSetting<Boolean> STRUCTURE_VELOCITY =
+            ofBoolean("structure_velocity", "settings.structure_velocity", true);
 
     private final T def;
     private final Class<T> type;
     private final String displayKey;
 
     private final String id;
+    private final List<T> values = new ArrayList<>();
 
     private PlayerSetting(String id, String displayKey, Class<T> type, T def) {
         this.displayKey = displayKey;
         this.def = def;
         this.type = type;
         this.id = id;
+    }
+
+    @SafeVarargs
+    private PlayerSetting(String id, String displayKey, Class<T> type, T def, T... values) {
+        this.displayKey = displayKey;
+        this.def = def;
+        this.type = type;
+        this.id = id;
+        this.values.addAll(Arrays.asList(values));
+    }
+
+    // Default Value Impl
+
+    private static PlayerSetting<Boolean> ofBoolean(String id, String displayKey, boolean def) {
+        return new PlayerSetting<>(id, displayKey, Boolean.class, def, true, false);
     }
 
     /**
@@ -64,6 +94,15 @@ public final class PlayerSetting<T> {
     @NotNull
     public Class<T> getType() {
         return type;
+    }
+
+    /**
+     * Fetches all of the possible values for this setting.
+     * @return List of Possible Values
+     */
+    @NotNull
+    public List<T> getPossibleValues() {
+        return ImmutableList.copyOf(values);
     }
 
     /**
@@ -110,5 +149,45 @@ public final class PlayerSetting<T> {
         }
 
         return "";
+    }
+
+    /**
+     * Creates an array of all of the Settings.
+     * @return Setting Values Array
+     */
+    @NotNull
+    public static PlayerSetting<?>[] values() {
+        List<PlayerSetting<?>> values = new ArrayList<>();
+
+        try {
+            for (Field f : PlayerSetting.class.getDeclaredFields()) {
+                if (!Modifier.isStatic(f.getModifiers())) continue;
+
+                if (PlayerSetting.class.isAssignableFrom(f.getType())) {
+                    f.setAccessible(true);
+                    values.add((PlayerSetting<?>) f.get(null));
+                }
+            }
+        } catch (ReflectiveOperationException e) {
+            StarConfig.print(e);
+        }
+
+        values.sort(Comparator.comparing(PlayerSetting::getId));
+
+        return values.toArray(new PlayerSetting[0]);
+    }
+
+    /**
+     * Fetches a Setting by its ID.
+     * @param id Setting ID
+     * @return Setting
+     */
+    public static PlayerSetting<?> byId(@Nullable String id) {
+        if (id == null) return null;
+
+        return Arrays.stream(values())
+                .filter(s -> s.getId().equalsIgnoreCase(id))
+                .findFirst()
+                .orElse(null);
     }
 }
