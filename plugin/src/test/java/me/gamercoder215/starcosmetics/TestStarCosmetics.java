7dd7764aec53e31092beab3e542b8ed829c02787
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-11-26 00:42:44 -0600

    Create TestStarCosmetics.java

---
+++ plugin/src/test/java/me/gamercoder215/starcosmetics/TestStarCosmetics.java
@@ -0,0 +1,18 @@
+package me.gamercoder215.starcosmetics;
+
+import me.gamercoder215.starcosmetics.api.cosmetics.pet.PetType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestStarCosmetics {
+
+    @Test
+    @DisplayName("Test Registry Checks")
+    public void testRegistry() {
+        for (PetType type : PetType.values())
+            if (StarCosmetics.PET_MAP.get(type) == null) Assertions.fail("Missing PetType: " + type.name());
+    }
+
+
+}
