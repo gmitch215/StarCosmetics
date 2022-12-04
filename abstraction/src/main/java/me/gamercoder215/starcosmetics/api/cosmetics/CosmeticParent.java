2a70948a70d4d5ced206617df35bd449677193f9
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-12-03 22:01:18 -0600

    Update CosmeticParent.java

--- abstraction/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/CosmeticParent.java
+++ abstraction/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/CosmeticParent.java
@@ -1,5 +1,6 @@
 package me.gamercoder215.starcosmetics.api.cosmetics;
 
+import me.gamercoder215.starcosmetics.api.cosmetics.trail.Trail;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 public enum CosmeticParent {
 
-    TRAILS(Material.ARROW, 20,
+    TRAILS(Material.ARROW, 20, Trail.class,
             BaseTrail.PROJECTILE_TRAIL, BaseTrail.GROUND_TRAIL, BaseTrail.SOUND_TRAIL),
 
     ;
@@ -16,37 +17,48 @@ public enum CosmeticParent {
     private final Material icon;
 
     private final Cosmetic[] children;
+
+    private final Class<? extends Cosmetic> childClass;
     private final String prefix;
 
     private final int place;
 
-    CosmeticParent(String prefix, Material icon, int place, Cosmetic... children) {
+    @SafeVarargs
+    <T extends Cosmetic> CosmeticParent(String prefix, Material icon, int place, Class<T> childClass, T... children) {
         this.prefix = prefix;
         this.icon = icon;
         this.children = children;
         this.place = place;
+        this.childClass = childClass;
     }
 
-    CosmeticParent(Material icon, int place, Cosmetic... children) {
-        this(ChatColor.YELLOW, icon, place, children);
+    @SafeVarargs
+    <T extends Cosmetic> CosmeticParent(Material icon, int place, Class<T> childClass, T... children) {
+        this(ChatColor.YELLOW, icon, place, childClass, children);
     }
 
-    CosmeticParent(ChatColor color, Material icon, int place, Cosmetic... children) {
-        this(color.toString(), icon, place, children);
+    @SafeVarargs
+    <T extends Cosmetic> CosmeticParent(ChatColor color, Material icon, int place, Class<T> childClass, T... children) {
+        this(color.toString(), icon, place, childClass, children);
     }
 
-    CosmeticParent(int hexColor, Material icon, int place, Cosmetic... children) {
+    @SafeVarargs
+    <T extends Cosmetic> CosmeticParent(int hexColor, Material icon, int place, Class<T> childClass, T... children) {
         this(ChatColor.translateAlternateColorCodes('&',
                 "&x&" + Integer.toHexString(hexColor).charAt(0) +
                         "&" + Integer.toHexString(hexColor).charAt(1) +
                         "&" + Integer.toHexString(hexColor).charAt(2) +
                         "&" + Integer.toHexString(hexColor).charAt(3) +
                         "&" + Integer.toHexString(hexColor).charAt(4) +
-                        "&" + Integer.toHexString(hexColor).charAt(5)), icon, place, children);
+                        "&" + Integer.toHexString(hexColor).charAt(5)), icon, place, childClass, children);
     }
 
     public String getPrefix() {
         return prefix;
+    }
+
+    public Class<? extends Cosmetic> getChildClass() {
+        return childClass;
     }
 
     public List<Cosmetic> getChildren() {
