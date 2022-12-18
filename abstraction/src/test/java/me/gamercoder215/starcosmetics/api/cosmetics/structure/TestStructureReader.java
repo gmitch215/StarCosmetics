7a209ec48e024a1bf2a8e3e0aab1281101dc4224
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-12-17 23:57:26 -0600

    Update TestStructureReader.java

--- abstraction/src/test/java/me/gamercoder215/starcosmetics/api/cosmetics/structure/TestStructureReader.java
+++ abstraction/src/test/java/me/gamercoder215/starcosmetics/api/cosmetics/structure/TestStructureReader.java
@@ -5,31 +5,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class TestStructureReader {
 
     @Test
     @DisplayName("Test Raw Points")
     public void testRawPoints() {
-        Assertions.assertEquals(new StructurePoint(1, 2, 3), StructureReader.readRawPoint("[1,2,3]"));
-        Assertions.assertEquals(new StructurePoint(-2, 5, 8), StructureReader.readRawPoint("[ -2, 5, 8 ]"));
-        Assertions.assertEquals(new StructurePoint(0, 0, 0), StructureReader.readRawPoint("[0, 0, 0]"));
+        Assertions.assertEquals(point(1, 2, 3), StructureReader.readRawPoint("[1,2,3]"));
+        Assertions.assertEquals(point(-2, 5, 8), StructureReader.readRawPoint("[ -2, 5, 8 ]"));
+        Assertions.assertEquals(point(0, 0, 0), StructureReader.readRawPoint("[0, 0, 0]"));
 
         Assertions.assertThrows(MalformedStructureException.class, () -> StructureReader.readRawPoint("[1,2,3"));
         Assertions.assertThrows(MalformedStructureException.class, () -> StructureReader.readRawPoint("1,2,3]"));
 
-        Assertions.assertEquals(new StructurePoint(3, 2, 1), StructureReader.readRawPoint("[3,2,1];"));
-        Assertions.assertEquals(new StructurePoint(3, 2, 1), StructureReader.readRawPoint("[3,2,1]; "));
+        Assertions.assertEquals(point(3, 2, 1), StructureReader.readRawPoint("[3,2,1];"));
+        Assertions.assertEquals(point(3, 2, 1), StructureReader.readRawPoint("[3,2,1]; "));
     }
 
     @Test
     @DisplayName("Test Multiple Raw Points")
     public void testMultipleRawPoints() {
         Assertions.assertEquals(
-                Lists.newArrayList(new StructurePoint(1, 2, 3), new StructurePoint(-2, 5, 8)),
+                Lists.newArrayList(point(1, 2, 3), point(-2, 5, 8)),
                 StructureReader.readPoints("[1,2,3]*[-2, 5, 8]")
         );
         Assertions.assertEquals(
-                Lists.newArrayList(new StructurePoint(1, 2, 3), new StructurePoint(-3, -9, 12), new StructurePoint(0, 0, 0)),
+                Lists.newArrayList(point(1, 2, 3), point(-3, -9, 12), point(0, 0, 0)),
                 StructureReader.readPoints("[1,2,3]*[-3, -9, 12]*[ 0, 0, 0 ]")
         );
 
@@ -37,11 +39,11 @@ public class TestStructureReader {
         Assertions.assertThrows(MalformedStructureException.class, () -> StructureReader.readPoints("[1,2,3*[-2, 5, 8]"));
 
         Assertions.assertEquals(
-                Lists.newArrayList(new StructurePoint(1, 2, 3), new StructurePoint(-2, 5, 8)),
+                Lists.newArrayList(point(1, 2, 3), point(-2, 5, 8)),
                 StructureReader.readPoints("[1,2,3]*[-2, 5, 8];")
         );
         Assertions.assertEquals(
-                Lists.newArrayList(new StructurePoint(1, 2, 3), new StructurePoint(-2, 5, 8)),
+                Lists.newArrayList(point(1, 2, 3), point(-2, 5, 8)),
                 StructureReader.readPoints("[1,2,3]*[-2, 5, 8]; ")
         );
     }
@@ -51,6 +53,16 @@ public class TestStructureReader {
     public void testCuboidPoints() {
         Assertions.assertEquals(9, StructureReader.readPoints("(1,-1,0,0,1,-1)^[1, 2, 3]").size());
         Assertions.assertEquals(18, StructureReader.readPoints("(1,-1,0,0,1,-1)^[1, 2, 3]*(1,-1,0,0,1,-1)^[-3,5,7]").size());
+
+        Assertions.assertEquals(Arrays.asList(
+                point(0, 5, 0), point(0, 5, 1), point(0, 5, 2),
+                point(1, 5, 0), point(1, 5, 1), point(1, 5, 2),
+                point(2, 5, 0), point(2, 5, 1), point(2, 5, 2)
+        ), StructureReader.readPoints("(1,-1,0,0,1,-1)^[0,5,0]"));
+    }
+
+    public static StructurePoint point(int x, int y, int z) {
+        return new StructurePoint(x, y, z);
     }
 
 }
