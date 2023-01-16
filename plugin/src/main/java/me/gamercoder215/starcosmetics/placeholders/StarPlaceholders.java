a265c0e869dc4476da4787d63a6ed3ca17478897
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-01-15 22:02:50 -0600

    Update Version to 1.0.2

--- plugin/src/main/java/me/gamercoder215/starcosmetics/placeholders/StarPlaceholders.java
+++ plugin/src/main/java/me/gamercoder215/starcosmetics/placeholders/StarPlaceholders.java
@@ -40,7 +40,7 @@ public class StarPlaceholders extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "1.0.1";
+        return "1.0.2";
     }
 
     // Impl
