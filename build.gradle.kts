7e38bb4f8b6ee599c31bce9998fe283a1c612ad2
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-30 10:02:47 -0400

    Update build.gradel.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -147,11 +147,9 @@ subprojects {
             options.memberLevel = JavadocMemberLevel.PROTECTED
         }
 
-        jar.configure {
+        jar {
             dependsOn("shadowJar")
-            artifacts {
-                add("default", getByName<ShadowJar>("shadowJar"))
-            }
+            archiveClassifier.set("dev")
         }
 
         withType<ShadowJar> {
@@ -164,12 +162,16 @@ subprojects {
             }
             exclude("META-INF", "META-INF/**")
 
-            relocate("revxrsal.commands", "me.gamercoder215.shaded.lamp")
-            relocate("org.bstats", "me.gamercoder215.shaded.bstats")
-            relocate("com.jeff_media.updatechecker", "me.gamercoder215.shaded.updatechecker")
+            relocate("revxrsal.commands", "me.gamercoder215.starcosmetics.shaded.lamp")
+            relocate("org.bstats", "me.gamercoder215.starcosmetics.shaded.bstats")
+            relocate("com.jeff_media.updatechecker", "me.gamercoder215.starcosmetics.shaded.updatechecker")
 
             archiveFileName.set("${project.name}-${project.version}.jar")
             archiveClassifier.set("")
         }
+    }
+
+    artifacts {
+        add("default", tasks.getByName<ShadowJar>("shadowJar"))
     }
 }
