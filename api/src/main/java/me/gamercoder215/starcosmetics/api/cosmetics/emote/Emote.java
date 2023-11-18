c70c02d246c09520ce17be103cee7028b8265747
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2023-11-17 19:58:46 -0600

    Add Emote Rarity and Criteria

--- api/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/emote/Emote.java
+++ api/src/main/java/me/gamercoder215/starcosmetics/api/cosmetics/emote/Emote.java
@@ -1,9 +1,14 @@
 package me.gamercoder215.starcosmetics.api.cosmetics.emote;
 
+import me.gamercoder215.starcosmetics.api.CompletionCriteria;
+import me.gamercoder215.starcosmetics.api.Rarity;
 import me.gamercoder215.starcosmetics.api.StarConfig;
+import org.bukkit.Statistic;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.InputStream;
+
+import static me.gamercoder215.starcosmetics.api.CompletionCriteria.fromStatistic;
 
 /**
  * Represents an Emote that a player can use.
@@ -13,14 +18,29 @@ public enum Emote {
     /**
      * The Wave Emote.
      */
-    WAVE,
+    WAVE(Rarity.COMMON),
     /**
      * The Dab Emote.
      */
-    DAB,
+    DAB(Rarity.OCCASIONAL, fromStatistic(Statistic.PLAYER_KILLS, 100)),
     ;
 
-    Emote() {}
+    /**
+     * The identifier tag for an Emote.
+     */
+    public static final String EMOTE_TAG = "emote";
+
+    private final Rarity rarity;
+    private final CompletionCriteria criteria;
+
+    Emote(Rarity rarity) {
+        this(rarity, CompletionCriteria.NONE);
+    }
+
+    Emote(Rarity rarity, CompletionCriteria criteria) {
+        this.criteria = criteria;
+        this.rarity = rarity;
+    }
 
     /**
      * Gets the input stream for the Emote File.
@@ -40,4 +60,19 @@ public enum Emote {
         return name().toLowerCase();
     }
 
+    /**
+     * Gets the rarity of the Emote.
+     * @return Emote Rarity
+     */
+    public Rarity getRarity() {
+        return rarity;
+    }
+
+    /**
+     * Gets the completion criteria of the Emote.
+     * @return Emote Completion Criteria
+     */
+    public CompletionCriteria getCriteria() {
+        return criteria;
+    }
 }
